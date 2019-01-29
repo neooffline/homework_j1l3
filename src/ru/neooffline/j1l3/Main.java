ea60805962b5e8eadeb3db33272b75eb97e8ef71
neooffline <admin@neooffline.ru>
2019-01-29 14:41:19 +0300

    Второе задание!

--- src/ru/neooffline/j1l3/Main.java
+++ src/ru/neooffline/j1l3/Main.java
@@ -4,43 +4,63 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-private static Scanner sc = new Scanner(System.in);
+static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-	String w1 = "banana";
-        for (int i = 0; i < toCharArray(w1).length;i++){
-            System.out.print(toCharArray(w1)[i] + "+");
-        }
-        String[] secret = {"baba"};
+
+        String[] secret = {"новый", "старый", "молодой", "больной", "ситцевый", "сереневый"};
         gameWithWords(secret);
     }
     static void gameWithWords(String[] arrayOfWords){
         Random random = new Random();
-        int randomElement = 0; //random.nextInt(arrayOfWords.length);
-        char[] charsRandomWord = new char[15];
-        char[] charsWord = new char[15];
-        System.arraycopy(arrayOfWords[randomElement],0,charsRandomWord,0,charsRandomWord.length);
-        boolean win=false;
+        int randomElement = random.nextInt(arrayOfWords.length);
+        char[] charsRandomWord;
+        char[] charsWord;
+        charsRandomWord=toCharArray(arrayOfWords[randomElement]);
         String res;
-        int l = 0;
+        System.out.println("\t\t\tСыграем в игру угадай слово!\n" +
+                "\tМы загадали случайное слово, а ты его должен угадать\n" +
+                "Внимание!! Длина слова не должна превышать 15 символов и " +
+                "\nсодержит только прописные русские буквы.\n" +
+                "\t\tНачнем!");
         do {
+            System.out.println("Введите своё слово: ");
             res= sc.next();
-            System.arraycopy(res,0,charsWord,0,charsWord.length);
-            for (int i = 0; i < charsWord.length; i++) {
+            charsWord=toCharArray(res);
+            for (int i = 0; i < charsRandomWord.length; i++) {
                 if (charsWord[i]!=charsRandomWord[i]){
                     charsWord[i]='#';
-                } else {
-                  l++;  
                 }
             }
-            for (char item:charsWord
-                 ) {
-                System.out.print(item+" ");
+            if (res.equals(arrayOfWords[randomElement])){
+                System.out.println("Поздравляем!! Вы победили!!!");
+                System.out.println("Загаданное слово - "+ arrayOfWords[randomElement]);
+            } else {
+                System.out.println("Попробуй еще раз");
+                printCharArray(charsWord);
             }
-        } while (l!=charsWord.length);
+        } while (!res.equals(arrayOfWords[randomElement]));
         System.out.println();
     }
     static char[] toCharArray(String word){
-        char[] charArray = word.toCharArray();
+        char[] charArray = new char[15];
+        if (word.length() < 15){
+            for (int i = 0; i < word.length(); i++) {
+                charArray[i] = word.charAt(i);
+            }
+            for (int i = word.length(); i < 15; i++) {
+                charArray[i] = '#';
+            }
+        } else {
+            for (int i = 0; i < 15; i++) {
+                charArray[i] = '#';
+            }
+        }
         return charArray;
+    }
+    static void printCharArray(char[] charArray){
+        for (char item:charArray) {
+            System.out.print(item);
+        }
+        System.out.println();
     }
 }
